1846d59f43646609476f45abe44a64276849042a
Divyanshu Shukla <shukladivyanshu745893@gmail.com>
2024-05-21 14:02:01 +0530

    Added EkaiInto and WhyEai components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import { EkaiIntro } from "./components/EkaiIntro";
+import { WhyEkai } from "./components/WhyEkai";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <EkaiIntro />
+      <WhyEkai />
+    </div>
+  );
 }
 
 export default App;
