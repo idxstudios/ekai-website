d9e56eb45af753de94884aa6da273d4c85a0dbf5
Divyanshu Shukla <shukladivyanshu745893@gmail.com>
2024-05-22 18:00:13 +0530

    Added work flow svg in EkaiWorkflow component

--- src/components/EkaiWorkflowComponent/index.js
+++ src/components/EkaiWorkflowComponent/index.js
@@ -1,10 +1,13 @@
 import "./style.scss";
+import ekaiWorkFlow from "../../assets/ekai-work-flow.svg";
 
 const EkaiWorkflowComponent = () => {
   return (
     <div className="workflow-wrapper">
       <h4>See how it works!</h4>
-      <div className="gif-container">Gif will show here</div>
+      <div className="gif-container">
+        <img src={ekaiWorkFlow} alt="Ekai Work Flow" />
+      </div>
     </div>
   );
 };
