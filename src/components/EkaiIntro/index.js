6161c30702ae0adec15d59e1a5877115fa8f5835
suraj dubey <dubeysuraj864@gmail.com>
2024-09-05 22:53:30 +0530

    changes in meta

--- src/components/EkaiIntro/index.js
+++ src/components/EkaiIntro/index.js
@@ -27,7 +27,7 @@ export const EkaiIntro = () => {
         <div className="left-cont">
 
           <div className="left-cont-heading">
-            AI {"{YOU}"} for the <br /> atters that don’t <br /> need real YOU.
+            AI {"{YOU}"} for <br /> matters that don’t <br /> need real YOU.
           </div>
           <p className="left-cont-subheading">
             Spend more time doing what matters.
