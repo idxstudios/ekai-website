0043c12e4eae0c49d5ef9fecc6e1d4f996905476
suraj dubey <dubeysuraj864@gmail.com>
2024-09-10 22:44:16 +0530

    changes

--- src/components/Pricing/index.js
+++ src/components/Pricing/index.js
@@ -1,40 +1,61 @@
-import React from 'react';
-import './styles.scss';
-import Square from '../../assets/CardSquare.svg';
+import React from "react";
+import "./styles.scss";
+import Square from "../../assets/CardSquare.svg";
 import Check from "../../assets/CheckCircle.svg";
 import Arrow from "../../assets/ArrowRight.svg";
 
-
 // Refactored Card component to accept props
-const Card = ({ title, subtitle, description, originalPrice, discountedPrice, pricingInfo, trialInfo, buttonText, benefits }) => {
+const Card = ({
+  title,
+  subtitle,
+  description,
+  originalPrice,
+  discountedPrice,
+  pricingInfo,
+  trialInfo,
+  buttonText,
+  benefits,
+}) => {
   return (
     <div className="card enterprise">
-      <div className="card-header">
-        <img src={Square} alt="Icon" className="card-icon" />
+      {/* <h3>{title}</h3> */}
+      <div>
+        <div className="card-header">
+          <img src={Square} alt="Icon" className="card-icon" />
+          <div>
+            <h5>{subtitle}</h5>
+            <h4>{title}</h4>
+          </div>
+        </div>{" "}
+        <p>{description}</p>
+        {benefits && (
+          <ul>
+            {benefits.map((benefit, index) => (
+              <li key={index}>
+                <img src={Check} alt="ekai" />
+                {benefit}
+              </li>
+            ))}
+          </ul>
+        )}
         <div>
-          <h5>{subtitle}</h5>
-          <h4>{title}</h4>
+          {" "}
+          {originalPrice && (
+            <p className="pricing-info">
+              <span className="original-price">{originalPrice}</span>
+              <span className="discounted-price">{discountedPrice}</span>
+            </p>
+          )}{" "}
+          <span className="pricingInfo">{pricingInfo}</span>
         </div>
       </div>
-      {/* <h3>{title}</h3> */}
-      <p>{description}</p>
-      {benefits && (
-        <ul>
-          {benefits.map((benefit, index) => (
-            <li key={index}><img src={Check} alt="ekai"/>{benefit}</li>
-          ))}
-        </ul>
-      )}
-      {originalPrice && (
-        <p className="pricing-info">
-          <span className="original-price">{originalPrice}</span>
-          <span className="discounted-price">{discountedPrice}</span>
-          
-        </p>
-      )}
-      <span className='pricingInfo'>{pricingInfo}</span>
-      {trialInfo && <p className="trial-info">{trialInfo}</p>}
-      <button className="contact-btn">{buttonText} <img src={Arrow} alt='ekai'/></button>
+
+     <div className="makeitcenter">
+     {trialInfo && <p className="trial-info">{trialInfo}</p>}
+      <button className="contact-btn">
+        {buttonText} <img src={Arrow} alt="ekai" />
+      </button>
+     </div>
     </div>
   );
 };
@@ -56,36 +77,37 @@ export default Pricing;
 
 const pricingCards = [
   {
-    title: 'Pilot',
-    subtitle: 'For individuals',
+    title: "Pilot",
+    subtitle: "For individuals",
     description:
-      'Ekai hosted plan ensures data protection and security on our cloud, not shared with other organizations.',
-    originalPrice: '$25',
-    discountedPrice: '$15',
-    pricingInfo: '/user per month',
-    trialInfo: '30 days free trial',
-    buttonText: 'Get started',
+      "Ekai hosted plan ensures data protection and security on our cloud, not shared with other organizations.",
+    originalPrice: "$25",
+    discountedPrice: "$15",
+    pricingInfo: "/user per month",
+    trialInfo: "30 days free trial",
+    buttonText: "Get started",
   },
   {
-    title: 'Pro',
-    subtitle: 'For individuals',
+    title: "Pro",
+    subtitle: "For individuals",
     description:
-      'Self-hosted plan with one-click installation, freedom to choose cloud provider, and preferred LLM.',
-    originalPrice: '$250',
-    discountedPrice: '$100',
-    pricingInfo: '/user annually',
-    trialInfo: ' ',
-    buttonText: 'Get started',
+      "Self-hosted plan with one-click installation, freedom to choose cloud provider, and preferred LLM.",
+    originalPrice: "$250",
+    discountedPrice: "$100",
+    pricingInfo: "/user annually",
+    trialInfo: " ",
+    buttonText: "Get started",
   },
   {
-    title: 'Enterprise',
-    subtitle: 'For big companies',
-    description: 'Includes all features from Pilot and Pro, with additional benefits:',
+    title: "Enterprise",
+    subtitle: "For big companies",
+    description:
+      "Includes all features from Pilot and Pro, with additional benefits:",
     benefits: [
-      'Ability to upload company-level documents from webpages, Wiki, or PDFs to train Ekai with basic knowledge.',
-      'Employee directory mapping.',
+      "Ability to upload company-level documents from webpages, Wiki, or PDFs to train Ekai with basic knowledge.",
+      "Employee directory mapping.",
     ],
-    trialInfo: ' ',
-    buttonText: 'Contact us',
+    trialInfo: " ",
+    buttonText: "Contact us",
   },
 ];
