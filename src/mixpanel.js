0b6f19f7b09b9e404a3cb1c49121c1e23074bfc5
suraj dubey <dubeysuraj864@gmail.com>
2024-09-14 17:19:31 +0530

    first commit

--- src/mixpanel.js
+++ src/mixpanel.js
@@ -1,6 +1,6 @@
 import mixpanel from "mixpanel-browser";
 
-mixpanel.init(process.env.REACT_APP_MIX_PANEL_TOKEN, { debug: true, })
+mixpanel.init(process.env.REACT_APP_MIX_PANEL_TOKEN, { debug: true, ignore_dnt: true })
 
 export const trackEvent = (eventName, properties = {}) => {
   mixpanel.track(eventName, properties);
